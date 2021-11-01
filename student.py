3f67208e12ddc339382d350ad97b28d333724d41
Matt Vanek <91546244+vanekm22@users.noreply.github.com>
2021-11-01 13:22:40 +0000

    sleeping

--- student.py
+++ student.py
@@ -58,6 +58,7 @@ class Piggy(PiggyParent):
     '''
     def doe(self):
       self.deg_fwd(360)
+      time.sleep(3)
       self.right(primary = 40, counter = -40)
       time.sleep(1)
       self.stop()
