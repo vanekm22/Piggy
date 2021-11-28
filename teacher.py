710f365cf6c10b76ffe9c9965ac16fa9499097e0
Matt Vanek <91546244+vanekm22@users.noreply.github.com>
2021-11-28 22:55:47 +0000

    sdfg

--- teacher.py
+++ teacher.py
@@ -121,10 +121,10 @@ class PiggyParent(gopigo3.GoPiGo3):
         # destination_angle = starting_angle + target_angle
         destination_angle = (starting_angle + target_angle) %360
         # determine the direction of the turn
-        if starting_angle < target_angle + starting_angle:
+        #if starting_angle < target_angle + starting_angle:
             turn_direction = "right"
-        else:
-            turn_direction = "left"
+        #else:
+         #   turn_direction = "left"
 
         # find initial error.
         error = abs(destination_angle - starting_angle)
