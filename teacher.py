865140a37c310b0b4378d9e6516ed19530fd0279
Matt Vanek <91546244+vanekm22@users.noreply.github.com>
2021-11-28 22:09:18 +0000

    0

--- teacher.py
+++ teacher.py
@@ -95,8 +95,8 @@ class PiggyParent(gopigo3.GoPiGo3):
                  top_speed = 80,
                  low_speed = 20,
                  kP  = 0.75,
-                 kI = 0.001, 
-                 kD = 0.001,
+                 kI = 0.0, 
+                 kD = 0.0,
                  acceptable_ending_error = 1):
 
         # Logfile of power added to my turn each frame 
